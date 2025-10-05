791510293471a728fe5ac9be88cb40d7f1095a90
Łukasz Kuczera <lukaszkuczera1@gmail.com>
2025-10-05 20:28:44 +0200

    v1.2 - UnitTests for Catalog

--- UnitTests/Program.cs
+++
@@ -1,9 +0,0 @@
-using UnitTests.REST_API_Tests;
-
-int delayMs = 200;
-Console.WriteLine($"Delay ms between steps: {delayMs}");
-
-var runner = new RESTAPI_Tests(string.Empty, delayMs: delayMs);
-await runner.PerformTestsAsync();
-
-Console.WriteLine("Done.");
